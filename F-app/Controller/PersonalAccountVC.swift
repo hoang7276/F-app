11f3753aaa0ba2417c0f14830db1e2927a09626c
Hoang <trghtutor@gmail.com>
2020-01-28 20:21:26 +0100

    Changed in design, worked on home page and personal account page

---
+++ F-app/Controller/PersonalAccountVC.swift
@@ -0,0 +1,30 @@
+//
+//  PersonalAccountVC.swift
+//  F-app
+//
+//  Created by Hoang Truong on 1/28/20.
+//  Copyright © 2020 Hoang Truong. All rights reserved.
+//
+
+import UIKit
+
+class PersonalAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ F-app/Model/DesignPost.swift
@@ -0,0 +1,9 @@
+//
+//  DesignPost.swift
+//  F-app
+//
+//  Created by Hoang Truong on 1/28/20.
+//  Copyright © 2020 Hoang Truong. All rights reserved.
+//
+
+import Foundation
